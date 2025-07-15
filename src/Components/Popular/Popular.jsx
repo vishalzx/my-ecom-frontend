dfe1094a68e7b9dea8f6258b316948c4a43af7d4
vishalzx <108291592+vishalzx@users.noreply.github.com>
2025-07-15 21:05:40 +0530

    code linked with backend

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import data_product from '../Assets/data'
+// import data_product from '../Assets/data'
 import Item from '../Item/Item'
 const Popular = () => {
   const [popularProducts, setPopularProducts]= useState([]);
@@ -14,7 +14,7 @@ const Popular = () => {
       <h1>POPULAR IN WOMEN</h1>
       <hr />
       <div className="popular-item">
-        {data_product.map((item,i)=>{
+        {popularProducts.map((item,i)=>{
           return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
         })}
       </div>
